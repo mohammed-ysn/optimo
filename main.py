227ee1ba7b11e5c69e0adec79bb6faaa9a4e48ad
Mohammed Yaseen <mohammedysn15@gmail.com>
2024-02-13 22:33:21 +0000

    Add main.py and simulation.py files for NS3 simulation

---
+++ main.py
@@ -0,0 +1,4 @@
+from simulation import NS3Simulation
+
+sim = NS3Simulation()
+sim.run()
--- simulation.py
+++ simulation.py
@@ -1,5 +1,4 @@
 import subprocess
-import sys
 
 import copy_scripts
 
@@ -31,6 +30,10 @@ class NS3Simulation:
         self.copy_scripts = False
 
     def set_tcp_variant(self, tcp_variant):
+        # Transport protocol to use: TcpNewReno, TcpLinuxReno,
+        # TcpHybla, TcpHighSpeed, TcpHtcp, TcpVegas, TcpScalable, TcpVeno,
+        # TcpBic, TcpYeah, TcpIllinois, TcpWestwoodPlus, TcpLedbat,
+        # TcpLp, TcpDctcp, TcpCubic, TcpBbr
         self.tcp_variant = tcp_variant
 
     def set_duration(self, duration):
@@ -57,42 +60,3 @@ class NS3Simulation:
         ns3_command = f'./ns-allinone-3.40/ns-3.40/ns3 run "scratch/{self.filename} --transport_prot={self.tcp_variant} --duration={self.duration} --tracing={str(self.tracing).lower()} --pcap_tracing={str(self.pcap_tracing).lower()}"'
         print(f"Running ns3 command: {ns3_command}")
         subprocess.run(ns3_command, shell=True)
-
-
-def main():
-    simulation = NS3Simulation()
-
-    if "--copy-scripts" in sys.argv:
-        simulation.enable_copy_scripts()
-        sys.argv.remove("--copy-scripts")
-
-    if "--tcp_variant" in sys.argv:
-        tcp_variant_index = sys.argv.index("--tcp_variant")
-        if len(sys.argv) > tcp_variant_index + 1:
-            simulation.set_tcp_variant(sys.argv[tcp_variant_index + 1])
-            sys.argv.pop(tcp_variant_index + 1)
-        sys.argv.remove("--tcp_variant")
-
-    if "--duration" in sys.argv:
-        duration_index = sys.argv.index("--duration")
-        if len(sys.argv) > duration_index + 1:
-            simulation.set_duration(int(sys.argv[duration_index + 1]))
-            sys.argv.pop(duration_index + 1)
-        sys.argv.remove("--duration")
-
-    if "--disable_tracing" in sys.argv:
-        simulation.disable_tracing()
-        sys.argv.remove("--disable_tracing")
-
-    if "--disable_pcap_tracing" in sys.argv:
-        simulation.disable_pcap_tracing()
-        sys.argv.remove("--disable_pcap_tracing")
-
-    if len(sys.argv) > 1:
-        simulation.set_filename(sys.argv[1])
-
-    simulation.run()
-
-
-if __name__ == "__main__":
-    main()
