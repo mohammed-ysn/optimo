da5e1275604673ed7d07e5aa324c4c418ca0724c
Mohammed Yaseen <mohammedysn15@gmail.com>
2024-02-13 22:22:45 +0000

    Add support for specifying TCP variant in ns3 command

--- run_ns3_simulation.py
+++ run_ns3_simulation.py
@@ -9,8 +9,19 @@ def main():
         copy_scripts.copy_cc_files("my_scripts", "./ns-allinone-3.40/ns-3.40/scratch")
         sys.argv.remove("--copy-scripts")
 
+    # Transport protocol to use: TcpNewReno, TcpLinuxReno,
+    # TcpHybla, TcpHighSpeed, TcpHtcp, TcpVegas, TcpScalable, TcpVeno,
+    # TcpBic, TcpYeah, TcpIllinois, TcpWestwoodPlus, TcpLedbat,
+    # TcpLp, TcpDctcp, TcpCubic, TcpBbr
+    tcp_variant = "TcpWestwoodPlus"
+    if "--tcp_variant" in sys.argv:
+        tcp_variant = sys.argv[sys.argv.index("--tcp_variant") + 1]
+        sys.argv.remove("--tcp_variant")
+        sys.argv.remove(tcp_variant)
+
     filename = sys.argv[1] if len(sys.argv) > 1 else "customfirst"
-    ns3_command = f"./ns-allinone-3.40/ns-3.40/ns3 run scratch/{filename}"
+    ns3_command = f'./ns-allinone-3.40/ns-3.40/ns3 run "scratch/{filename} --transport_prot={tcp_variant} --duration=3 --tracing=true --pcap_tracing=true"'
+    print(ns3_command)
 
     print(f"Running ns3 command: {ns3_command}")
     subprocess.run(ns3_command, shell=True)
