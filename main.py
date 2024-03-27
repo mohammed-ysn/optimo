08d7aa87ac3b1d5dae8e4baa2466eea3fe2177d3
Mohammed Yaseen <mohammedysn15@gmail.com>
2024-03-27 17:00:51 +0000

    Add command line argument support to main.py

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+import argparse
 from simulation import NS3Simulation
 
-sim = NS3Simulation("tcp-comparison", copy_scripts=True)
-sim.run()
+
+def main():
+    parser = argparse.ArgumentParser(description="Run NS3Simulation")
+    parser.add_argument(
+        "--copy-scripts", action="store_true", help="Copy scripts flag (default: False)"
+    )
+    args = parser.parse_args()
+
+    copy_scripts = args.copy_scripts
+
+    sim = NS3Simulation("tcp-comparison", copy_scripts=copy_scripts)
+    sim.run()
+
+
+if __name__ == "__main__":
+    main()
